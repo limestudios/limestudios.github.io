0ed8c57699ec45d26834f56cc57b43ef644ea809
Liam Craver <studios.lime@gmail.com>
2015-02-09 11:16:20 -0600

    Added active styles

--- assets/js/sort.js
+++ assets/js/sort.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   $('.category-button').click(function(){
      var clickedItemID = $(this).attr('id');
+     $('.category-button').removeClass('active');
+     $(this).addClass('active');
      if(clickedItemID === 'all') {
        $('.portfolio-element').removeClass('active');
        $('.portfolio-element').addClass('active');
