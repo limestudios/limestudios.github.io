5dd831e680da040238316a67d83aa57121243d0e
Liam Craver <studios.lime@gmail.com>
2015-09-25 03:02:10 -0500

    added footer

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,15 +1,25 @@
 $(function() {
     //caches a jQuery object containing the header element
     var header = $(".header-position");
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-        header.height(Math.max(64, Math.min(256-scroll, 256)));
-        if(header.height() <= 64) {
-          header.removeClass("normal").addClass("small");
-        } else {
-          header.removeClass("small").addClass("normal");
-        }
-    });
+    var homeSections = $(".home-sections");
+    console.log(homeSections.length);
+    if($(window).height() < 640) {
+      header.removeClass("normal").addClass("small");
+      header.height(64);
+    } else if(homeSections.length > 0) {
+        $(window).scroll(function() {
+            var scroll = $(window).scrollTop();
+            header.height(Math.max(64, Math.min(256-scroll, 256)));
+            if(header.height() <= 64 || $(window).height() < 800) {
+              header.removeClass("normal").addClass("small");
+            } else {
+              header.removeClass("small").addClass("normal");
+            }
+        });
+    } else {
+      header.removeClass("normal").addClass("small");
+      header.height(64);
+    }
 
   // handle links with @href started with '#' only
   $(document).on('click', 'a[href^="#"]', function(e) {
