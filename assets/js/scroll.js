51b296c85ff8a50a5447657b7c938607d38a7df1
Liam Craver <studios.lime@gmail.com>
2015-09-25 03:28:16 -0500

    fixed header bugs

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -2,15 +2,19 @@ $(function() {
     //caches a jQuery object containing the header element
     var header = $(".header-position");
     var homeSections = $(".home-sections");
+    var homeWrap = $(".home-wrap");
     console.log(homeSections.length);
     if($(window).height() < 640) {
-      header.removeClass("normal").addClass("small");
+      header.removeClass("normal").addClass("small").addClass("small-screen");
+      homeSections.removeClass("normal").addClass("small");
+      homeWrap.removeClass("normal").addClass("small");
       header.height(64);
     } else if(homeSections.length > 0) {
         $(window).scroll(function() {
             var scroll = $(window).scrollTop();
-            header.height(Math.max(64, Math.min(256-scroll, 256)));
-            if(header.height() <= 64 || $(window).height() < 800) {
+            if(!header.hasClass("small-screen"))
+              header.height(Math.max(64, Math.min(256-scroll, 256)));
+            if(header.height() <= 64) {
               header.removeClass("normal").addClass("small");
             } else {
               header.removeClass("small").addClass("normal");
@@ -20,6 +24,21 @@ $(function() {
       header.removeClass("normal").addClass("small");
       header.height(64);
     }
+
+    $( window ).resize(function() {
+      if($(window).height() < 640) {
+        header.removeClass("normal").addClass("small").addClass("small-screen");
+        homeSections.removeClass("normal").addClass("small");
+        homeWrap.removeClass("normal").addClass("small");
+        header.height(64);
+      } else {
+        header.removeClass("small").addClass("normal").removeClass("small-screen");
+        homeSections.removeClass("small").addClass("normal");
+        homeWrap.removeClass("small").addClass("normal");
+        var scroll = $(window).scrollTop();
+        header.height(Math.max(64, Math.min(256-scroll, 256)));
+      }
+    });
 
   // handle links with @href started with '#' only
   $(document).on('click', 'a[href^="#"]', function(e) {
